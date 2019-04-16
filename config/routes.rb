c0c97d78d7c029b204e7168126afdbc33e8275da
Daniel Ko <dankomong@gmail.com>
2019-04-16 16:18:29 -0400

    First commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :battles
   resources :bosses
-  resources :characters
+  resources :characters, only: [:index]
   resources :jobs
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
