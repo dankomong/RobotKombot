c09b3e9117b8085a8d6dd06d618613d071f2c150
DanFCo <danfcorreia@gmail.com>
2019-04-18 13:56:36 -0400

    delete

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,61 +1,65 @@
 class CharactersController < ApplicationController
 
-before_action :get_character_by_id, only: [:show, :edit, :update, :destroy]
+  before_action :get_character_by_id, only: [:show, :edit, :update, :destroy]
 
 
-def index
-  @characters = Character.all
-end
+  def index
+    @characters = Character.all
+  end
 
 
-def new
-  @character = Character.new
-end
+  def new
+    @character = Character.new
+  end
 
 
-def create
-  @character = Character.create(character_params(:name))
-  if @character.valid?
-  redirect_to character_path(@character)
-else
-  render :new
-end
-end
-
-# def update
-#   @character.update(character_params)
-#   if character.valid?
-#   redirect_to character_path(@character)
-# else
-#   render :edit
-# end
-# end
+  def create
+    @character = Character.create(character_params(:name))
+    if @character.valid?
+      redirect_to character_path(@character)
+    else
+      render :new
+    end
+  end
 
 
+  def edit
+  end
 
-def destroy
-  @character.destroy
-  redirect_to characters_path
-end
+  def update
+    @character.update(character_params(:name))
+    if character.valid?
+      redirect_to character_path(@character)
+    else
+      render :edit
+    end
+  end
 
 
 
+  def destroy
+    @character.destroy
+    redirect_to characters_path
+  end
 
 
 
-private
 
 
-def get_character_by_id
+
+  private
+
+
+  def get_character_by_id
     @character = Character.find(params[:id])
-end
+  end
 
 
 
 
-def character_params(*args)
-  params.require(:character).permit(*args)
-end
+  def character_params(*args)
+    params.require(:character).permit(*args)
+  end
 
 
 
