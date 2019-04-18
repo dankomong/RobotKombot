e03b40939b7fc38dcac6ace5a329df755ae71b22
Daniel Ko <dankomong@gmail.com>
2019-04-18 16:28:21 -0400

    push with master

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,10 +1,7 @@
 class CharactersController < ApplicationController
 
   before_action :get_character_by_id, only: [:show, :edit, :update, :destroy]
-<<<<<<< HEAD
   before_action :authorized?
-=======
->>>>>>> 4204aa8f284ce552cc631ef83819a42c47b535a5
 
 
   def index
@@ -15,32 +12,6 @@ class CharactersController < ApplicationController
     @character = Character.new
   end
 
-<<<<<<< HEAD
-  def create
-    @character = Character.new(character_params(:name, :job_id))
-    @character.user_id = session[:user_id]
-    @character.update_stats
-    if @character.valid?
-
-      redirect_to character_path(@character)
-    else
-      render :new
-    end
-  end
-
-  def show
-
-  end
-
-  # def update
-  #   @character.update(character_params)
-  #   if character.valid?
-  #   redirect_to character_path(@character)
-  # else
-  #   render :edit
-  # end
-  # end
-=======
   def show
 
   end
@@ -73,7 +44,6 @@ class CharactersController < ApplicationController
       render :edit
     end
   end
->>>>>>> 4204aa8f284ce552cc631ef83819a42c47b535a5
 
 
 
@@ -86,24 +56,10 @@ class CharactersController < ApplicationController
   private
 
 
-<<<<<<< HEAD
   def get_character_by_id
     @character = Character.find(params[:id])
   end
 
-=======
-
-
-
-
-  def get_character_by_id
-    @character = Character.find(params[:id])
-  end
-
-
-
-
->>>>>>> 4204aa8f284ce552cc631ef83819a42c47b535a5
   def character_params(*args)
     params.require(:character).permit(*args)
   end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
-      sessions[:user_id] = @user.id
+      session[:user_id] = @user.id
       redirect_to jobs_path
     # else
     #   flash["errors"] = ["Your username or password was incorrect"]
