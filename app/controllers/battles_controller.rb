7f7d671c47b1afd716b4300310d0cd11b3fe4f50
DanFCo <danfcorreia@gmail.com>
2019-04-18 18:28:11 -0400

    Battle Render

--- app/controllers/battles_controller.rb
+++ app/controllers/battles_controller.rb
@@ -2,43 +2,48 @@ class BattlesController < ApplicationController
   before_action :authorized?
 
 
-def index
-  @battles = Battle.all
-end
-
-def show
-
-  @battle = Battle.find(params[:id])
-  @percent1 = "100"
-  @percent2 = "100"
-  # @boss = Boss.find(@battle.boss)
-  # @character = Character.find(@battle.character.id)
-end
-
-def create
-  @battle = Battle.create(battle_params)
-end
-
-def fight
-  @battle = Battle.find(params[:id])
-  @character = Character.find(@battle.character_id)
-  @percent1 = (params[:percent1].to_i - rand(1..20)).to_s
-  @percent2 = (params[:percent2].to_i - rand(1..20)).to_s
-  if @percent1.to_i <= 0
-    redirect_to characters_path
-  elsif @percent2.to_i <= 0
-    redirect_to characters_path
-  else
-    render :show
+  def index
+    @battles = Battle.all
   end
 
-end
+  def show
 
-private
+    @battle = Battle.find(params[:id])
+    @percent1 = "100"
+    @percent2 = "100"
+    # @boss = Boss.find(@battle.boss)
+    # @character = Character.find(@battle.character.id)
+  end
 
-def battle_params
-  params.require(:battle).permit(:name, :victory, :boss_id, :character)
-end
+  def create
+    @battle = Battle.create(battle_params)
+  end
+
+  def fight
+    @battle = Battle.find(params[:id])
+    @character = Character.find(@battle.character_id)
+
+    @percent1 = (params[:percent1].to_i - rand(1..20)).to_s
+    @percent2 = (params[:percent2].to_i - rand(1..20)).to_s
+    if @percent1.to_i <= 0 || @percent2.to_i <= 0
+      if @percent1.to_i <= 0
+        @percent1 = "0"
+      elsif @percent2.to_i <= 0
+        @percent2 = "0"
+      end
+
+      render :show
+    else
+      render :show
+    end
+
+  end
+
+  private
+
+  def battle_params
+    params.require(:battle).permit(:name, :victory, :boss_id, :character)
+  end
 
 
 
